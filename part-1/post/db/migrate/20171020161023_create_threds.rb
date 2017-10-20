c10872451f4fbe6be298d746dcf7691ac2ea3199
Michelle Cinello <m.cinello@gmail.com>
2017-10-20 12:17:55 -0400

    added models

--- part-1/post/db/migrate/20171020161023_create_threds.rb
+++ part-1/post/db/migrate/20171020161023_create_threds.rb
@@ -1,6 +1,9 @@
 class CreateThreds < ActiveRecord::Migration[5.1]
   def change
     create_table :threds do |t|
+      t.string :title
+      t.integer :forum_id
+      t.integer :user_id
 
       t.timestamps
     end
